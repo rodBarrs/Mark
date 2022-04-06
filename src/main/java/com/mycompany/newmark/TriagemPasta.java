5b91ff21589746d8e6d71b1292f02352eb085686
Rafael Henrique <fel.rique70@gmail.com>
2022-04-06 14:00:03 -0300

    Triagem Pastas

--- src/main/java/com/mycompany/newmark/TriagemPasta.java
+++ src/main/java/com/mycompany/newmark/TriagemPasta.java
@@ -17,7 +17,7 @@ import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-public class Triagem_Pasta {
+public class TriagemPasta {
 
     public Chaves_Resultado documentoPasta(WebDriver driver, WebDriverWait wait, Chaves_Configuracao config, String bancos)
             throws InterruptedException, UnsupportedFlavorException, IOException, SQLException {
@@ -32,6 +32,9 @@ public class Triagem_Pasta {
         String condicaoCabecalho = "CAB";
         String localTriagem = "DOC";
         String processo = "";
+        int diferenca = 30;
+        boolean existeSeguinte = false;
+        int proximaLinha = 0;
         VerificaCondicaoPastas verificaCondicaoPastas = new VerificaCondicaoPastas();
 
         wait.until(ExpectedConditions.presenceOfElementLocated(By.id("treeview-1015")));
@@ -42,6 +45,7 @@ public class Triagem_Pasta {
         int limite = 10;
         WebElement movimentacaoAtual;
 
+
         for (int i = listaMovimentacao.size(); i > 0 && limite > 0; i--) {
 
 
@@ -49,106 +53,151 @@ public class Triagem_Pasta {
                     .contains("x-tree-expander");
             if (existePasta) {
                 movimentacaoAtual = driver.findElement(By.xpath("//tr[" + i + "]/td[2]/div"));
+
+
+
+
                 if (verificaCondicaoPastas.verificaCondicao(movimentacaoAtual.getText())) {
-                    int proximaLinha = Integer
-                            .parseInt(driver.findElement(By.xpath("//tr[" + (i + 1) + "]/td/div")).getText()) + 1;
+                    int numeroMovimentacao = Integer.parseInt(driver.findElement(By.xpath(("/html/body/div[2]/div[2]/div/div[2]/div/div[3]/div/table/tbody/tr["+ i +"]/td[1]/div"))).getText());
+                    try {
+                        existeSeguinte = driver.findElement(By.xpath("/html/body/div[2]/div[2]/div/div[2]/div/div[3]/div/table/tbody/tr["+ (i+1) +"]/td[1]/div")).getAttribute("class").contains("x-grid-cell");
+                        if(existeSeguinte) {
+                            int numeroMovimentacaoSeguinte = Integer.parseInt(driver.findElement(By.xpath(("/html/body/div[2]/div[2]/div/div[2]/div/div[3]/div/table/tbody/tr[" + (i + 1) + "]/td[1]/div"))).getText());
+
+                            diferenca = numeroMovimentacaoSeguinte - numeroMovimentacao;
+                        }
+                    } catch (Exception e){
+
+                    }
+
+
+                    System.out.println(movimentacaoAtual);
+                    System.out.println(numeroMovimentacao);
+                   // System.out.println(numeroMovimentacaoSeguinte);
+
+
 
                     wait.until(ExpectedConditions.invisibilityOfElementLocated(By.id("ext-gen1020")));
                     wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//tr[\" + i + \"]/td/div/span/span[1]")));
                     driver.findElement(By.xpath("//tr[" + i + "]/td[2]/div/img[1]")).click();
-
-                    for (int j = i ; j < proximaLinha; j++) {
-                        pdf.apagarPDF();
-                        wait.until(ExpectedConditions
-                                .presenceOfElementLocated(By.xpath("//tr[" + j + "]/td[2]/div/span/span[1]")));
-                        wait.until(ExpectedConditions
-                                .elementToBeClickable(By.xpath("//tr[" + j + "]/td[2]/div/span/span[1]")));
-
-                        Thread.sleep(500);
-
-                        wait.until(
-                                ExpectedConditions.invisibilityOfElementLocated(By.xpath("//*[@id=\"ext-gen1124\"]")));
-
-                        // Armazena o span que contém o texto onde diz se o item é um PDF ou HTML
-                        String spanText = driver.findElement(By.xpath("//tr[" + j + "]/td[2]/div/span/span[2]"))
-                                .getText().toUpperCase();
-
-                        // Verifica se o documento é um pdf para tratamento apropriado
-                        if (spanText.contains("PDF")) {
-                            pdf.apagarPDF();
-                            // Click na linha
-                            driver.findElement(By.xpath("//tr[" + j + "]/td[2]/div/span/span[1]")).click();
-
-                            int cont = 0;
-                            while (cont <= 2) {
-
-                                if (pdf.PDFBaixado()) {
-                                    processo = pdf.lerPDF();
-                                    break;
-                                } else {
-                                    pdf.apagarPDF();
-                                    driver.findElement(By.xpath("//tr[" + j + "]/td[2]/div/span/span[1]")).click();
-                                }
-                                cont++;
-                            }
-
-                        } else {
-                            driver.findElement(By.xpath("//tr[" + j + "]/td[2]/div/span/span[1]")).click();
-                            // Envia o driver para o iframe e verifca os itens internos para confirmação do
-                            // carregamento
-                            wait.until(ExpectedConditions.presenceOfElementLocated(By.id("iframe-myiframe")));
-                            WebElement iframe = driver.findElement(By.id("iframe-myiframe"));
-                            wait.until(ExpectedConditions.frameToBeAvailableAndSwitchToIt(iframe));
-                            // Garante o clique no iframe
-                            boolean flag = true;
-                            do {
-                                try {
-                                    wait.until(ExpectedConditions.elementToBeClickable(By.tagName("html")));
-                                    wait.until(ExpectedConditions.presenceOfElementLocated(By.tagName("body")));
-                                    driver.findElement(By.tagName("body")).click();
-                                    flag = false;
-                                } catch (Exception e) {
-                                    // Nothing to do at all
-                                }
-
-                            } while (flag);
-                            Actions action = new Actions(driver);
-                            action.keyDown(Keys.CONTROL).sendKeys(String.valueOf('\u0061')).perform();
-                            action.keyDown(Keys.CONTROL).sendKeys(String.valueOf('\u0063')).perform();
-                            driver.switchTo().defaultContent();
-                            Clipboard clipboard = Toolkit.getDefaultToolkit().getSystemClipboard();
-                            DataFlavor flavor = DataFlavor.stringFlavor;
-                            Thread.sleep(500);
-                            processo = clipboard.getData(flavor).toString();
-                        }
-
-                        if (processo.length() > 1) {
-
-                            limite--;
-                            try {
-                                Boolean identificadoDePeticao = false;
-                                resultado = triagem.triarBanco(processo, bancos, localTriagem, config.getTipoTriagem(),
-                                        identificadoDePeticao);
-                                if (!resultado.getEtiqueta()
-                                        .contains("NÃO FOI POSSÍVEL LOCALIZAR FRASE CHAVE ATUALIZADA")
-                                        && !resultado.getEtiqueta().contains("ERRO EM TRIAGEM: PDF NÃO PESQUISÁVEL")) {
-                                    linhaMovimentacao = driver.findElement(By.xpath("//tr[" + i + "]/td/div"))
-                                            .getText();
-                                    resultado.setLocal("DOC " + linhaMovimentacao);
-                                    resultado.setDriver(driver);
-                                    return resultado;
-                                }
-                            } catch (Exception erro) {
-                                wait.until(ExpectedConditions.presenceOfElementLocated(By.id("button-1005-btnEl")));
-                                driver.findElement(By.id("button-1005-btnEl")).click();
-                                erro.printStackTrace();
-                            }
-
-                        } else {
-                            resultado.setEtiqueta("ERRO EM TRIAGEM: INSTABILIDADE NO SAPIENS");
-                        }
+                    if  (!existeSeguinte){
+                        WebElement TabelaTref2 = driver.findElement(By.id("treeview-1015"));
+                        List<WebElement> listaMovimentacao2 = new ArrayList(TabelaTref.findElements(By.cssSelector("tr")));
+                        int numeroUltimaMovimentacao = Integer.parseInt(driver.findElement(By.xpath(("/html/body/div[2]/div[2]/div/div[2]/div/div[3]/div/table/tbody/tr[" + listaMovimentacao2.size() + "]/td[1]/div"))).getText());
+                        diferenca = (numeroUltimaMovimentacao - numeroMovimentacao) + 1;
 
                     }
+
+                        //proximaLinha = Integer
+                        //        .parseInt(driver.findElement(By.xpath("//tr[" + (i + 1) + "]/td/div")).getText()) + 1;
+
+
+
+
+                        int contador = 0;
+                    for (int j = i ; j < 1000; j++) {
+
+
+
+                            pdf.apagarPDF();
+                            wait.until(ExpectedConditions
+                                    .presenceOfElementLocated(By.xpath("//tr[" + j + "]/td[2]/div/span/span[1]")));
+                            wait.until(ExpectedConditions
+                                    .elementToBeClickable(By.xpath("//tr[" + j + "]/td[2]/div/span/span[1]")));
+
+                            Thread.sleep(500);
+
+                            wait.until(
+                                    ExpectedConditions.invisibilityOfElementLocated(By.xpath("//*[@id=\"ext-gen1124\"]")));
+
+                            // Armazena o span que contém o texto onde diz se o item é um PDF ou HTML
+                            String spanText = driver.findElement(By.xpath("//tr[" + j + "]/td[2]/div/span/span[2]"))
+                                    .getText().toUpperCase();
+
+                            // Verifica se o documento é um pdf para tratamento apropriado
+                            if (spanText.contains("PDF")) {
+                                pdf.apagarPDF();
+                                // Click na linha
+                                driver.findElement(By.xpath("//tr[" + j + "]/td[2]/div/span/span[1]")).click();
+
+                                int cont = 0;
+                                while (cont <= 4) {
+
+                                    if (pdf.PDFBaixado()) {
+                                        processo = pdf.lerPDF();
+                                        break;
+                                    } else {
+                                        pdf.apagarPDF();
+                                        driver.findElement(By.xpath("//tr[" + j + "]/td[2]/div/span/span[1]")).click();
+                                    }
+                                    cont++;
+                                }
+
+                            } else {
+                                driver.findElement(By.xpath("//tr[" + j + "]/td[2]/div/span/span[1]")).click();
+                                // Envia o driver para o iframe e verifca os itens internos para confirmação do
+                                // carregamento
+                                wait.until(ExpectedConditions.presenceOfElementLocated(By.id("iframe-myiframe")));
+                                WebElement iframe = driver.findElement(By.id("iframe-myiframe"));
+                                wait.until(ExpectedConditions.frameToBeAvailableAndSwitchToIt(iframe));
+                                // Garante o clique no iframe
+                                boolean flag = true;
+                                do {
+                                    try {
+                                        wait.until(ExpectedConditions.elementToBeClickable(By.tagName("html")));
+                                        wait.until(ExpectedConditions.presenceOfElementLocated(By.tagName("body")));
+                                        driver.findElement(By.tagName("body")).click();
+                                        flag = false;
+                                    } catch (Exception e) {
+                                        // Nothing to do at all
+                                    }
+
+                                } while (flag);
+                                Actions action = new Actions(driver);
+                                action.keyDown(Keys.CONTROL).sendKeys(String.valueOf('\u0061')).perform();
+                                action.keyDown(Keys.CONTROL).sendKeys(String.valueOf('\u0063')).perform();
+                                driver.switchTo().defaultContent();
+                                Clipboard clipboard = Toolkit.getDefaultToolkit().getSystemClipboard();
+                                DataFlavor flavor = DataFlavor.stringFlavor;
+                                Thread.sleep(500);
+                                processo = clipboard.getData(flavor).toString();
+                            }
+
+                            if (processo.length() > 1) {
+
+                                limite--;
+                                try {
+                                    Boolean identificadoDePeticao = false;
+                                    resultado = triagem.triarBanco(processo, bancos, localTriagem, config.getTipoTriagem(),
+                                            identificadoDePeticao);
+                                    if (!resultado.getEtiqueta()
+                                            .contains("NÃO FOI POSSÍVEL LOCALIZAR FRASE CHAVE ATUALIZADA")
+                                            && !resultado.getEtiqueta().contains("ERRO EM TRIAGEM: PDF NÃO PESQUISÁVEL")) {
+                                        linhaMovimentacao = driver.findElement(By.xpath("//tr[" + i + "]/td/div"))
+                                                .getText();
+                                        resultado.setLocal("DOC " + linhaMovimentacao);
+                                        resultado.setDriver(driver);
+                                        return resultado;
+                                    }
+                                } catch (Exception erro) {
+                                    wait.until(ExpectedConditions.presenceOfElementLocated(By.id("button-1005-btnEl")));
+                                    driver.findElement(By.id("button-1005-btnEl")).click();
+                                    erro.printStackTrace();
+                                }
+
+                            } else {
+                                resultado.setEtiqueta("ERRO EM TRIAGEM: INSTABILIDADE NO SAPIENS");
+                            }
+                            contador++;
+
+
+                                if (contador == diferenca) {
+                                    break;
+                                }
+
+                        }
+
+
                     }
 
             }
--- src/main/java/com/mycompany/newmark/Triagem_Etiquetas.java
+++ src/main/java/com/mycompany/newmark/Triagem_Etiquetas.java
@@ -1,14 +1,10 @@
 /**
- * @author Felipe Marques, Gabriel Ramos, Rafael Henrique e Adriano Vilhena 
- * 
+ * @author Felipe Marques, Gabriel Ramos, Rafael Henrique e Adriano Vilhena
+ *
  */
 package com.mycompany.newmark;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.sql.*;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -16,9 +12,8 @@ import javax.swing.JOptionPane;
 
 public class Triagem_Etiquetas {
 
-
 	public Chaves_Resultado triarBanco(String processo, String banco, String localtriagem, String tipoTriagem,
-			Boolean identificadorDePeticao) {
+									   Boolean identificadorDePeticao) {
 		Chaves_Resultado resultado = new Chaves_Resultado();
 		Tratamento tratamento = new Tratamento();
 		processo = tratamento.tratamento(processo);
@@ -26,52 +21,59 @@ public class Triagem_Etiquetas {
 		while (bancoBool == false) {
 			try {
 				Connection connection = DriverManager.getConnection("jdbc:sqlite:BancoEtiquetasMark.db");
+
 				PreparedStatement stmt;
 				ResultSet resultSet;
-				if (identificadorDePeticao) {
 
+				if (identificadorDePeticao) {
 					stmt = connection.prepareStatement("SELECT * FROM identificador_materia ORDER BY prioridade DESC");
 					//public boolean peticaoTrue = true;
-				} else {
 
-					if (banco.contains("TODOS OS BANCOS")) {
-						stmt = connection.prepareStatement(
-								"SELECT * FROM etiquetas WHERE tipo = '" + localtriagem + "' ORDER BY prioridade DESC");
-					} else {
-						stmt = connection.prepareStatement("SELECT * FROM etiquetas WHERE banco = '" + banco
-								+ "' AND tipo = '" + localtriagem + "' ORDER BY prioridade DESC");
-					}
+				} else {
+					stmt = connection.prepareStatement(
+							"SELECT * FROM etiquetas WHERE tipo = '" + localtriagem + "' ORDER BY prioridade DESC");
 				}
+
 				resultSet = stmt.executeQuery();
+
 				while (resultSet.next()) {
 					String PALAVRACHAVE = resultSet.getString("palavrachave");
-					PALAVRACHAVE = tratamento.tratamento(PALAVRACHAVE);				
+					PALAVRACHAVE = tratamento.tratamento(PALAVRACHAVE);
+					if (PALAVRACHAVE.equals("HISTÓRICO DE LAUDOS MÉDICOS PERICIAIS")){
+						System.out.println(PALAVRACHAVE);
+					}
 					String COMPLEMENTO = resultSet.getString("complemento");
 					COMPLEMENTO = tratamento.tratamento(COMPLEMENTO);
-					
-					if (processo.contains(PALAVRACHAVE) && processo.contains(COMPLEMENTO)) {
-						//resultado.setId(resultSet.getInt("id"));
-						resultado.setPalavraChave(resultSet.getString("id"));
-						if (identificadorDePeticao){
-							resultado.setSubnucleo(resultSet.getString("subnucleo"));
-
-						}
-						resultado.setEtiqueta(resultSet.getString("etiqueta"));
-
+					String BANCO = resultSet.getString("banco");
+					BANCO.replace(" ","");
+					String bancoComSpacee = banco + " ";
+					System.out.println(BANCO);
+					System.out.println(banco);
+					if (BANCO.equalsIgnoreCase(banco) || BANCO.equalsIgnoreCase(bancoComSpacee)){
+						if (processo.contains(PALAVRACHAVE) && processo.contains(COMPLEMENTO)) {
+							//resultado.setId(resultSet.getInt("id"));
+							resultado.setPalavraChave(resultSet.getString("id"));
+							if (identificadorDePeticao) {
+								resultado.setSubnucleo(resultSet.getString("subnucleo"));
+							}
+							resultado.setEtiqueta(resultSet.getString("etiqueta"));
 //						resultSet.getString("etiqueta");
 //						if (resultSet.wasNull()) {
 //							resultado.setEtiqueta(resultSet.getString("subnucleo"));
-//						} else {							
+//						} else {
 //							resultado.setEtiqueta(resultSet.getString("subnucleo") + "/" + resultSet.getString("etiqueta"));
 //						}
-						
-						connection.close();
-						return resultado;
+
+							connection.close();
+							return resultado;
+						}
 					}
+
 
 				}
 				connection.close();
 				bancoBool = true;
+
 			} catch (SQLException ex) {
 				bancoBool = false;
 				Logger.getLogger(Triagem_Etiquetas.class.getName()).log(Level.SEVERE, null, ex);
